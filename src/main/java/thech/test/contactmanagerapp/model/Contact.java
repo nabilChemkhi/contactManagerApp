a35444437cb39631c27882efc586d95204d28889
nabilChemkhi <nabil.chemkhi@esprit.tn>
2023-11-05 21:37:48 +0100

    first commit

---
+++ src/main/java/thech/test/contactmanagerapp/model/Contact.java
@@ -0,0 +1,6 @@
+package thech.test.contactmanagerapp.model;
+
+public class Contact {
+    private Long id;
+    private String name;
+}
---
+++ src/test/java/thech/test/contactmanagerapp/TechTestApplicationTests.java
@@ -0,0 +1,13 @@
+package thech.test.contactmanagerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
